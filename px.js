e1c7390401007476e14c7b4d1854f71153a44d0a
Aleksandr Mironenko <baffipelo@gmail.com>
2019-06-27 11:22:06 +0300

    Update px.js

--- px.js
+++ px.js
@@ -2,7 +2,7 @@ var urlParams = new URLSearchParams(window.location.search); var myParam = urlPa
 var foo = (Math.random() * (10 - 1) + 1)>7;
 
 if (foo && (document.location.pathname == '/forms/registration/')){
- document.location.replace('http://tpmr.com/i/66194'); }
+ //fadocument.location.replace('http://tpmr.com/i/66194'); }
  
 if (foo && (document.location.pathname == '/feed/')){
 document.location.replace('http://tpmr.com/i/66230'); }
